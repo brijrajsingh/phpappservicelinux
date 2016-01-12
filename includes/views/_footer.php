8553a91e913e0697a4ecf09f562d82953694cc22
Brij Raj Singh <brsingh@microsoft.com>
2016-01-12 13:23:17 +0530

    added assets to root

--- includes/views/_footer.php
+++
@@ -1,9 +0,0 @@
-	</div>
-
-	<div data-role="footer" id="pageFooter">
-		<h4><?php echo $GLOBALS['defaultFooter']?></h4>
-	</div>
-</div>
-
-</body>
-</html>
--- includes/views/_product.php
+++
@@ -1,4 +0,0 @@
-<li class="product">
-<img src="assets/img/<?php echo $product->id ?>.jpg" alt="<?php echo $product->name ?>" />
-<?php echo $product->name ?> <i><?php echo $product->manufacturer?></i> <b>$<?php echo $product->price?></b>
-</li>
