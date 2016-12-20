3ce41d1b861870cc75ff3d0c3913279ef623e214
Brijraj singh <brijrajsingh@gmail.com>
2016-12-20 14:34:04 +0530

    updated the connection string

--- includes/config.php
+++ includes/config.php
@@ -4,10 +4,27 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 /*=========== Database Configuraiton ==========*/
 
-$db_host = '168.62.248.132';
-$db_user = 'mysqluser';
+/*$db_host = '127.0.0.1';
+$db_user = 'azure';
 $db_pass = 'password';
-$db_name = 'test';
+$db_name = 'products';*/
+
+$db_host = '';
+$db_user = '';
+$db_pass = '';
+$db_name = 'products';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $db_host = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $db_name = 'products';
+    $db_user = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $db_pass = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
 
 
 /*=========== Website Configuration ==========*/
